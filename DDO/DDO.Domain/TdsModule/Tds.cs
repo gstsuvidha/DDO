76b71f6da50944c728ab400fb571232b8aacf4d1
Vishal Rathore <vishalrathor9211@gmail.com>
2018-09-24 11:27:58 +0530

    done

---
+++ DDO/DDO.Domain/TdsModule/Tds.cs
@@ -0,0 +1,81 @@
+using System;
+using DDO.Domain.Accounting;
+using DDO.Domain.AdminModule;
+using DDO.Domain.Inferface;
+using DDO.Domain.SupplierModule;
+
+namespace DDO.Domain.TdsModule
+{
+    public class Tds: IHaveAccountingUnit, IHaveDateFilter
+    {
+        public int Id { get; set; }
+        public Supplier Supplier { get; set; }
+
+        public int SupplierId { get; set; }
+        public DateTime Date { get; set; }
+
+        
+
+        public string PlaceOfSupply { get; set; }
+
+        public double AmountPaid { get; set; }
+
+        public double CgstAmount { get; set; }
+        public double SgstAmount { get; set; }
+        public double IgstAmount { get; set; }
+        public double TdsAmount  { get; set; }
+
+        public bool IsActive { get; set; }  
+
+        public AccountingUnit AccountingUnit {get; set;}
+
+        public string AccountingUnitId {get; set;}
+        public Admin Admin { get; set; }
+        public int AdminId { get; set; }
+
+    public Tds()
+    {
+        
+    }
+
+
+    public Tds(int supplierId, DateTime date, string placeOfSupply, double amountPaid, double cgstAmount,
+                double sgstAmount, double igstAmount, double tdsAmount, string accountingUnitId, int adminId)
+    {
+        SupplierId = supplierId;
+        Date = date;
+        PlaceOfSupply = placeOfSupply;
+        AmountPaid = amountPaid;
+        CgstAmount = cgstAmount;
+        SgstAmount = sgstAmount;
+        IgstAmount = igstAmount;
+        TdsAmount = tdsAmount;
+        AccountingUnitId = accountingUnitId;
+        AdminId = adminId;
+            IsActive = true;
+
+  }
+
+     public void Modify(int supplierId, DateTime date, string placeOfSupply, double amountPaid, double cgstAmount,
+                double sgstAmount, double igstAmount, double tdsAmount, string accountingUnitId)
+    {
+        SupplierId = supplierId;
+        Date = date;
+        PlaceOfSupply = placeOfSupply;
+        AmountPaid = amountPaid;
+        CgstAmount = cgstAmount;
+        SgstAmount = sgstAmount;
+        IgstAmount = igstAmount;
+        TdsAmount = tdsAmount;
+        AccountingUnitId = accountingUnitId;
+        IsActive = true;
+
+  }
+     
+        public void Delete()
+        {
+            IsActive = false;
+        }
+
+    }
+}
---
+++ DDO/DDO.WebApp/ClientApp/src/app/components/tds/Itds.ts
@@ -0,0 +1,18 @@
+export interface Itds {
+
+    id:number;
+    supplierId: number;
+    placeOfSupply: string;
+    amountPaid:number;
+    cgstAmount: number;
+    sgstAmount: number;
+    igstAmount: number;
+    tdsAmount: number;
+    date: Date;
+}
+
+
+
+
+
+
